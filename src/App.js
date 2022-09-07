b53c061475de2500607a3745e174cfbf1092a01f
“matthewhasemeier” <mshweb22@gmail.com>
2022-09-07 07:10:19 -0700

    first commiot for react first project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <code>src/App.js</code>
         </p>
         <a
           className="App-link"
@@ -17,8 +17,16 @@ function App() {
         >
           Learn React
         </a>
+        <h1>This is an H1</h1>
+        <p>This is a paragraph element</p>
+        <ul>
+          <li>This is a list item</li>
+          <li>Another list item</li>
+          <li>yet another list item</li>
+        </ul>
       </header>
     </div>
+    
   );
 }
 
